24a820bfe466d44ee593d797e2eb8f1561408ea7
Akseli Nurmio <akseli@nurmio.fi>
2018-12-28 13:30:36 +0200

    Add manifest file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,16 @@
 module.exports = {
 	plugins: [
 		'gatsby-plugin-react-helmet',
+		{
+			resolve: 'gatsby-plugin-manifest',
+			options: {
+				name: 'Akseli Nurmio',
+				start_url: '/',
+				theme_color: '#ffff00',
+				background_color: '#000000',
+				icon: 'src/images/favicon.svg',
+			},
+		},
 		'gatsby-plugin-offline',
 		{
 			resolve: 'gatsby-plugin-canonical-urls',
