70ac067d099e8f5c00011ad76a2e5a335841f449
Akseli Nurmio <akseli@nurmio.fi>
2019-08-14 23:27:17 +0300

    Add SEO component

---
+++ src/components/SEO.tsx
@@ -0,0 +1,76 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+
+interface SEOProps {
+  title: string
+  lang?: string
+  description?: string
+  image?: string
+  imageAlt?: string
+}
+
+const SEO: React.FunctionComponent<SEOProps> = ({
+  title,
+  lang = "en",
+  description,
+  image,
+  imageAlt,
+}) => {
+  const meta: JSX.IntrinsicElements["meta"][] = [
+    {
+      property: "og:type",
+      content: "website",
+    },
+    {
+      name: "twitter:card",
+      content: "summary",
+    },
+    {
+      property: "og:title",
+      content: title,
+    },
+  ]
+
+  if (description) {
+    meta.push(
+      {
+        name: "description",
+        content: description,
+      },
+      {
+        property: "og:description",
+        content: description,
+      },
+    )
+  }
+
+  if (image) {
+    meta.push(
+      {
+        property: "og:image",
+        content: image,
+      },
+      {
+        property: "twitter:image",
+        content: image,
+      },
+    )
+
+    if (imageAlt) {
+      meta.push(
+        {
+          property: "og:image:alt",
+          content: imageAlt,
+        },
+        {
+          property: "twitter:image:alt",
+          content: imageAlt,
+        },
+      )
+    }
+  }
+
+  return <Helmet htmlAttributes={{ lang }} title={title} meta={meta} />
+}
+
+export default SEO
