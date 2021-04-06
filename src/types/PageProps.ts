72c7073de351c965e1618d3755f9631edd4c7ac7
Akseli Nurmio <akseli@nurmio.fi>
2021-04-06 17:02:50 +0300

    Give page data object some typings

--- src/types/PageProps.ts
+++ src/types/PageProps.ts
@@ -1,10 +1,21 @@
-export interface PageProps {
-  location: {
-    pathname: string
-    search: string
-    hash: string
+import type { PageProps } from "gatsby"
+import type { FixedObject, FluidObject } from "gatsby-image"
+
+export type { PageProps }
+
+interface FixedImageData {
+  childImageSharp: {
+    fixed: FixedObject
   }
 }
-export interface PagePropsWithData extends PageProps {
-  data: any
+interface FluidImageData {
+  childImageSharp: {
+    fluid: FluidObject
+  }
 }
+
+export type WorkPageProps = PageProps<{
+  heroImage: FixedImageData
+  slushImage: FixedImageData & FluidImageData
+  hdwImage: FixedImageData & FluidImageData
+}>
