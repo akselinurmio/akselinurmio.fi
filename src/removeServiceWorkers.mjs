4a15c00df5db4b674da6d6a992a9873d12807d31
Akseli Nurmio <akseli@nurmio.fi>
2023-01-10 12:52:58 +0200

    Remove service worker removal script as it didn't work

--- src/removeServiceWorkers.mjs
+++
@@ -1,9 +0,0 @@
-(async () => {
-  if (!navigator.serviceWorker) return;
-
-  const registrations = await navigator.serviceWorker.getRegistrations();
-
-  for (const registration of registrations) {
-    registration.unregister();
-  }
-})();
