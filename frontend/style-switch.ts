38bb0fad0b20d9da9da8c119a24236fc11c9f590
Akseli Nurmio <akseli@nurmio.fi>
2024-07-23 21:59:25 +0300

    Add 90s alternative styles

---
+++ frontend/style-switch.ts
@@ -0,0 +1,28 @@
+function activate90sStylesheet() {
+  const defaultStylesheet = document.getElementById(
+      "main-stylesheet",
+    ) as HTMLLinkElement,
+    alternateStylesheet = document.getElementById(
+      "ninetees-stylesheet",
+    ) as HTMLLinkElement;
+
+  alternateStylesheet.disabled = false;
+  defaultStylesheet.disabled = true;
+}
+
+function initialize90sSwitch() {
+  const button = document.getElementById("to-90s") as HTMLButtonElement;
+
+  button.hidden = false;
+
+  button.addEventListener("click", () => {
+    activate90sStylesheet();
+    button.hidden = true;
+  });
+}
+
+function start90sTimer() {
+  setTimeout(initialize90sSwitch, 10_000);
+}
+
+window.addEventListener("load", start90sTimer);
